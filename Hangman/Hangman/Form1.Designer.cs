560771f9f3f6dcaab7c666cfe592511a32578b1c
Douglas Shirilla <douglasshirilla@yahoo.com>
2017-10-08 20:55:20 -0400

    Add files via upload

---
+++ Hangman/Hangman/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.LetterEnterLabel = new System.Windows.Forms.Label();
+            this.letterGuessBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.prevGuessDisplay = new System.Windows.Forms.Label();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.answerTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.guessesLeftLab = new System.Windows.Forms.Label();
+            this.guessNumLab = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Monotype Corsiva", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(150, 22);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(195, 43);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "HANGMAN";
+            // 
+            // LetterEnterLabel
+            // 
+            this.LetterEnterLabel.AutoSize = true;
+            this.LetterEnterLabel.Location = new System.Drawing.Point(31, 105);
+            this.LetterEnterLabel.Name = "LetterEnterLabel";
+            this.LetterEnterLabel.Size = new System.Drawing.Size(78, 13);
+            this.LetterEnterLabel.TabIndex = 1;
+            this.LetterEnterLabel.Text = "Guess a letter: ";
+            // 
+            // letterGuessBox
+            // 
+            this.letterGuessBox.Location = new System.Drawing.Point(115, 102);
+            this.letterGuessBox.MaxLength = 1;
+            this.letterGuessBox.Name = "letterGuessBox";
+            this.letterGuessBox.Size = new System.Drawing.Size(100, 20);
+            this.letterGuessBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Previous guesses:";
+            // 
+            // prevGuessDisplay
+            // 
+            this.prevGuessDisplay.AutoSize = true;
+            this.prevGuessDisplay.Location = new System.Drawing.Point(34, 153);
+            this.prevGuessDisplay.Name = "prevGuessDisplay";
+            this.prevGuessDisplay.Size = new System.Drawing.Size(0, 13);
+            this.prevGuessDisplay.TabIndex = 4;
+            // 
+            // guessButton
+            // 
+            this.guessButton.Location = new System.Drawing.Point(101, 200);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(75, 23);
+            this.guessButton.TabIndex = 5;
+            this.guessButton.Text = "GUESS!";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // answerTextBox
+            // 
+            this.answerTextBox.Location = new System.Drawing.Point(70, 229);
+            this.answerTextBox.Name = "answerTextBox";
+            this.answerTextBox.ReadOnly = true;
+            this.answerTextBox.Size = new System.Drawing.Size(145, 20);
+            this.answerTextBox.TabIndex = 6;
+            this.answerTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox.Location = new System.Drawing.Point(269, 99);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(193, 150);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 7;
+            this.pictureBox.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(383, 293);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "By: Douglas Shirilla";
+            // 
+            // guessesLeftLab
+            // 
+            this.guessesLeftLab.AutoSize = true;
+            this.guessesLeftLab.Location = new System.Drawing.Point(34, 170);
+            this.guessesLeftLab.Name = "guessesLeftLab";
+            this.guessesLeftLab.Size = new System.Drawing.Size(72, 13);
+            this.guessesLeftLab.TabIndex = 9;
+            this.guessesLeftLab.Text = "Guesses Left:";
+            // 
+            // guessNumLab
+            // 
+            this.guessNumLab.AutoSize = true;
+            this.guessNumLab.Location = new System.Drawing.Point(112, 170);
+            this.guessNumLab.Name = "guessNumLab";
+            this.guessNumLab.Size = new System.Drawing.Size(0, 13);
+            this.guessNumLab.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 315);
+            this.Controls.Add(this.guessNumLab);
+            this.Controls.Add(this.guessesLeftLab);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.answerTextBox);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.prevGuessDisplay);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.letterGuessBox);
+            this.Controls.Add(this.LetterEnterLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label LetterEnterLabel;
+        private System.Windows.Forms.TextBox letterGuessBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label prevGuessDisplay;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.TextBox answerTextBox;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label guessesLeftLab;
+        private System.Windows.Forms.Label guessNumLab;
+    }
+}
+
---
+++ Hangman/Hangman/Form1.cs
@@ -0,0 +1,237 @@
+﻿//Programmer: Douglas Shirilla
+//Date: 04/25/2017
+//Course: COP-2360
+//Purpose: As a final project for the C# language class, I created a GUI hangman game application.
+//The user has 10 guesses to guess the word which is selected at random from a .txt file 
+//containing a list of random words. If guessed letter is in the selected word, the guess doesn't
+//count against the user and the hidden word displays where the letter is.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Console;
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+        //declare needed variables with global scope
+        private string chosenWord = "";
+        private int maxGuesses = 10;
+        private int numOfGuesses = 0;
+        private string hiddenWord = "";
+        private string prevGuesses = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //get set for chosenWord
+        public string theWord
+        {
+            get { return chosenWord; }
+            set
+            {
+                chosenWord = value;
+            }
+        }
+
+        //get set for hiddenWord
+        public string theHiddenWord
+        {
+            get { return hiddenWord; }
+            set { hiddenWord = value; }
+        }
+
+        //method to pull and fill chosenWord from .txt file
+        public String createWord()
+        {
+
+            //replace this string with your own .txt file of words
+            String path = @"C:\Users\Douglas\Desktop\School\COP-2360\Project\words.txt";
+
+            var lines = File.ReadAllLines(path);
+            var r = new Random();
+            var randomLineNumber = r.Next(0, lines.Length - 1);
+            var line = lines[randomLineNumber];
+            chosenWord = line.ToString();
+            return chosenWord;
+        }
+
+        //method to create hiddenWord and use & characters to block out answer
+        public String createHiddenWord()
+        {
+            int numOfletters = chosenWord.Length;
+            for (int i = 0; i < numOfletters; i++)
+            {
+                hiddenWord += '&';
+            }
+            return hiddenWord;
+        }
+
+
+        //sets the textbox to hiddenWord
+        public void setAnswer(String hiddenWord)
+        {
+            answerTextBox.Text = hiddenWord;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //initialize first round of fields for user
+            theWord = createWord();
+            theHiddenWord = createHiddenWord();
+            setAnswer(theHiddenWord);
+            guessNumLab.Text = Convert.ToString(maxGuesses);
+        }
+
+        //method to change the hiddenWord to incorporate a correct guess from user
+        public void modifyHiddenWord(String input)
+        {
+            char usersGuess = Convert.ToChar(input);
+            int index = chosenWord.IndexOf(input);
+
+            char[] b = chosenWord.ToCharArray();
+            char[] a = hiddenWord.ToCharArray();
+            for(int i = 0;i < hiddenWord.Length;i++)
+            {
+                if(b[i] == usersGuess)
+                {
+                    a[i] = usersGuess;
+                }
+            }
+            
+            hiddenWord = new string(a);
+            answerTextBox.Text = hiddenWord;
+
+        }
+
+
+        //button click triggered event
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            string input = null;
+            input = letterGuessBox.Text;
+            int numOfletters = chosenWord.Length;
+            
+            //check for valid input
+            if (input == "")
+            {
+                MessageBox.Show("Field cannot be blank!");
+            }
+            else if(prevGuesses.Contains(input)==true)
+            {
+                MessageBox.Show("You already guessed that letter!");
+            }
+
+            //if input is valid and not blank
+            else if(chosenWord.Contains(input)==true)
+            {
+                char[] word = chosenWord.ToCharArray();
+                char usersGuess = Convert.ToChar(input);
+                prevGuesses += input;
+                prevGuessDisplay.Text = prevGuesses;
+                modifyHiddenWord(input);
+                if(hiddenWord.Equals(chosenWord))
+                {
+                    MessageBox.Show("YOU WON!");
+                    setNewGame();
+                } 
+            }
+
+            //code block for guess that is valid and not in the hiddenWord
+            else
+            {
+                numOfGuesses += 1;
+                guessNumLab.Text = Convert.ToString(maxGuesses - numOfGuesses);
+                prevGuesses += input;
+                prevGuessDisplay.Text = prevGuesses;
+            }
+
+            //sets picturebox to image of hangman
+            if(numOfGuesses > 0)
+            {
+                setPictureBox(numOfGuesses);
+            }
+            
+            //if user runs out of guesses
+            if (numOfGuesses == maxGuesses)
+            {
+                MessageBox.Show(String.Format("YOU LOSE! The correct word was {0}", chosenWord));
+                setNewGame();
+            }
+            
+        }
+
+        //method to set GUI for new game and reset counters
+        public void setNewGame()
+        {
+            numOfGuesses = 0;
+            pictureBox.Image = null;
+            prevGuessDisplay.Text = "";
+            prevGuesses = " ";
+            chosenWord = "";
+            hiddenWord = "";
+            guessNumLab.Text = Convert.ToString(maxGuesses);
+            letterGuessBox.Text = "";
+            theWord = createWord();
+            theHiddenWord = createHiddenWord();
+            setAnswer(theHiddenWord);
+        }
+
+
+        //method to determine what picture to show in picturebox
+        public void setPictureBox(int numOfGuesses)
+        {
+            if(numOfGuesses == 1)
+            {
+                pictureBox.Image = Properties.Resources.hangmanBody1;
+            }
+            else if(numOfGuesses == 2)
+            {
+                pictureBox.Image = Properties.Resources.hangmanBody2;
+            }
+            else if(numOfGuesses == 3)
+            {
+                pictureBox.Image = Properties.Resources.hangmanBody3;
+            }
+            else if(numOfGuesses == 4)
+            {
+                pictureBox.Image = Properties.Resources.hangmanBody4;
+            }
+            else if(numOfGuesses == 5)
+            {
+                pictureBox.Image = Properties.Resources.hangmanBody5;
+            }
+            else if(numOfGuesses == 6)
+            {
+                pictureBox.Image = Properties.Resources.HangmanBody6;
+            }
+            else if(numOfGuesses == 7)
+            {
+                pictureBox.Image = Properties.Resources.hangmanBody7;
+            }
+            else if(numOfGuesses == 8)
+            {
+                pictureBox.Image = Properties.Resources.hangmanBody8;
+            }
+            else if(numOfGuesses == 9)
+            {
+                pictureBox.Image = Properties.Resources.hangmanBody9;
+            }
+            else
+            {
+                pictureBox.Image = Properties.Resources.hangmanBody10;
+            }
+        }
+    }
+}
